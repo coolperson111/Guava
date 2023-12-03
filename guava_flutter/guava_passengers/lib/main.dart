aa4a9b3e5b3aa05636291e9e3bc97e156a5b97fb
coolperson111 <chakalguk@gmail.com>
2023-12-03 13:40:57 +0530

    firebase setup

--- guava_flutter/guava_passengers/lib/main.dart
+++ guava_flutter/guava_passengers/lib/main.dart
@@ -1,22 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:guava_passengers/authentication/signup_screen.dart';
+import 'package:guava_passengers/authentication/login_screen.dart';
 
-void main() {
-    runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-    @override
-        Widget build(BuildContext context) {
-            return MaterialApp(
-                    title: 'Flutter Demo',
-                    debugShowCheckedModeBanner: false,
-                    theme: ThemeData.dark().copyWith(
-                            scaffoldBackgroundColor: Colors.black,
-                        ),
-                    home: SignUpScreen(),
-                    );
-        }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const LogInScreen(),
+    );
+  }
 }
